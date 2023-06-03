6936f7f511ea0a05a0c5babc2e3af1d3880c7c4f
Adhitya <83760410+ashura-code@users.noreply.github.com>
2023-06-03 23:19:13 +0530

    Updated whatsapp.py
    by adhitya

--- whatsapp.py
+++ whatsapp.py
@@ -3,7 +3,7 @@ import pyautogui as pg
 import time
 import pyinputplus as pyip
 
-no = pyip.inputNum('enter phone no: ', min=10,max=100)
+no = pyip.inputNum('enter phone no: ', min=10,max=10)
 phone_no = "+91 ", no
 message = input("enter the message: ")
 web.open(f"https://web.whatsapp.com/send?phone={phone_no}&text={message}")
