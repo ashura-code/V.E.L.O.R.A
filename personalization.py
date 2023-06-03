096c983e35caa147522834aff2d9b6df7f7d0dab
Srivardhani14 <135311543+Srivardhani14@users.noreply.github.com>
2023-06-03 13:55:36 +0530

    Updated to dynamic wp
    by sri

--- personalization.py
+++ personalization.py
@@ -1,4 +1,5 @@
 import ctypes
+import datetime
 
 
 # Function to set the desktop background image
@@ -10,11 +11,21 @@ def set_wallpaper(image_path):
     result = user32.SystemParametersInfoW(20, 0, image_path, 3)
 
     if result:
-        print("Desktop background set successfully.")
+        print(f"Desktop background set successfully. Image: {image_path}")
     else:
         print("Failed to set desktop background.")
 
 
-# Example usage
-image_path = "C:/Users/sriva/Desktop/pics.jpg"
+# Get the current time
+current_time = datetime.datetime.now().time()
+
+# Determine the image path based on the current time
+if current_time < datetime.time(12):
+    image_path = "C:/Users/sriva/Downloads/emma-simpson-mNGaaLeWEp0-unsplash.jpg"
+elif current_time < datetime.time(18):
+    image_path = "C:/Users/sriva/Downloads/pngwing.com.png"
+else:
+    image_path = "C:/Users/sriva/Desktop/pic/pics.jpg"
+
+# Set the wallpaper
 set_wallpaper(image_path)
